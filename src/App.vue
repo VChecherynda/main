f47572d1924c8682748b2023dac5d2ca700c10f0
vchecherynda <vchecherynda@shutterstock.com>
2024-01-09 02:21:56 +0200

    Add small fixes and port

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,60 @@
 <template> 
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <MainComponent msg="This is Vue main app" :styles="styles" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MainComponent from "./components/main-component.vue";
 
 export default {
+  id: "app",
   name: "App",
   components: {
-    HelloWorld,
+    MainComponent,
+  },
+  data() {
+    return {
+      theme: null,
+      styles: null
+    };
+  },
+  watch: {
+    theme() {
+      const getBackgroundColor = () => {
+        switch (this.theme) {
+          case "blue":
+            return "#9BBEC8";
+          case "green":
+            return "#93B1A6";
+          default:
+            return "initial";
+        }
+      };
+      this.styles = {
+        height: '100%',
+        padding: "20px",
+        color: "white",
+        background: getBackgroundColor()
+      };
+    },
+  },
+  mounted() {
+    this.theme = 'blue';
+    this.handleThemeChange = this.handleThemeChange.bind(this);
+    window.addEventListener("changeTheme", this.handleThemeChange);
+  },
+  beforeUnmount() {
+    window.removeEventListener("changeTheme", this.handleThemeChange);
+  },
+  methods: {  
+    handleThemeChange(evt) {
+      this.theme = evt.detail.theme;
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/main-component.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :style="computedStyles" class="hello">{{ msg }}</div>
+</template>
+
+<script>
+
+export default {
+  name: "MainComponent",
+  props: {
+    msg: String,
+    styles: Object
+  },
+  computed: {
+    computedStyles() {
+      return this.styles;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
